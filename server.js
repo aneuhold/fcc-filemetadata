d3cc4e239a13bf2b1e59957290e3db467f994d51
Anton G. Neuhold Jr <agneuhold@gmail.com>
2018-12-21 10:34:06 -0800

    Update server.js
    Added the working server.js code. Copy pasted from glitch where it was running.

--- server.js
+++ server.js
@@ -2,21 +2,29 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+const multer = require('multer');
 
 var app = express();
 
-app.use(cors());
+const upload = multer({dest: 'uploads/'});
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
